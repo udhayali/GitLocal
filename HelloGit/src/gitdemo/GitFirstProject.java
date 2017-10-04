b82313adcdb3516f3e203e0bd021eca330f9b6ea
udhayalibom <rayyavu@yahoo.com>
2017-10-04 00:35:03 -0400

    Hello Git

---
+++ HelloGit/src/gitdemo/GitFirstProject.java
@@ -0,0 +1,12 @@
+package gitdemo;
+
+public class GitFirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Hello Git");
+
+	}
+
+}
